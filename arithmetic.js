40566ee563046ff8415ad3c0cf87a13c63ecbc9d
DavidCai <376462191@qq.com>
2015-03-28 10:43:16 +0800

    选择排序

---
+++ arithmetic.js
@@ -0,0 +1,14 @@
+var moment =require('moment');
+var selectionSort = require('./lib/selectionSort');
+var _ = require('lodash');
+
+var arrayToSort = [];
+
+//生成的随机数数组的长度
+var NUMBER_OF_THE_ARRAY_TO_SORT = 1000;
+
+for(var i = 0 ; i < NUMBER_OF_THE_ARRAY_TO_SORT ; i++){
+	arrayToSort.push(_.random(9999));
+}
+
+console.log(selectionSort.sort(arrayToSort));
