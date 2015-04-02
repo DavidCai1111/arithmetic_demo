21115c7c3f94b6bd697f2bc8b8e1a056c702bb0b
DavidCai <376462191@qq.com>
2015-04-02 15:18:53 +0800

    gulp coffee编译

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,21 @@ var coffee = require('gulp-coffee');
 
 var coffee_src = "./lib/coffee/*.coffee";
 var coffee_dest = "./lib/coffee_dest";
+var coffee_arrUtilSrc = "./lib/coffee/arrUtil/*.coffee";
+var coffee_arrUtilDest = "./lib/coffee_dest/arrUtil";
 
 gulp.task('coffee_make',function(){
 	gulp
 		.src(coffee_src)
 		.pipe(coffee())
-		.pipe(gulp.dest(coffee_dest));
-
+		.pipe(gulp.dest(coffee_dest))
 });
 
-gulp.task('default',['coffee_make']);
+gulp.task('coffee_arrUtil_make',function(){
+	gulp
+		.src(coffee_arrUtilSrc)
+		.pipe(coffee())
+		.pipe(gulp.dest(coffee_arrUtilDest));
+});
+
+gulp.task('default',['coffee_make','coffee_arrUtil_make']);
