84d726cc93ad71ecd74345d551d5a79318d7ca93
DavidCai <376462191@qq.com>
2015-04-04 16:35:22 +0800

    coffee

--- config.coffee
+++ config.coffee
@@ -3,6 +3,6 @@ config = {}
 #生成的随机数数组的长度
 config.NUMBER_OF_THE_ARRAY_TO_SORT = 10000;
 #生成的随机数的范围
-config .RANGE_OF_RANDOM_NUMBER = 9999;
+config.RANGE_OF_RANDOM_NUMBER = 9999;
 
 module.exports = config
--- gulpfile.js
+++ gulpfile.js
@@ -2,33 +2,33 @@ var gulp = require('gulp');
 var coffee = require('gulp-coffee');
 
 var coffeeConfig = {
-	arithSrc:"./lib/coffee/*.coffee",
-	arithDest:"./lib/coffee_dest",
-	arrUtilSrc:"./lib/coffee/arrUtil/*.coffee",
-	arrUtilDest:"./lib/coffee_dest/arrUtil",
-	rootPathFileSrc:"./*.coffee",
-	rootPathFileDest:"./"
+	arithSrc: "./lib/coffee/*.coffee",
+	arithDest: "./lib/coffee_dest",
+	arrUtilSrc: "./lib/coffee/arrUtil/*.coffee",
+	arrUtilDest: "./lib/coffee_dest/arrUtil",
+	rootPathFileSrc: "./*.coffee",
+	rootPathFileDest: "./"
 };
 
-gulp.task('coffee_make',function(){
+gulp.task('coffee_make', function () {
 	gulp
 		.src(coffeeConfig.arithSrc)
 		.pipe(coffee())
 		.pipe(gulp.dest(coffeeConfig.arithDest));
 });
 
-gulp.task('coffee_arrUtil_make',function(){
+gulp.task('coffee_arrUtil_make', function () {
 	gulp
 		.src(coffeeConfig.arrUtilSrc)
 		.pipe(coffee())
 		.pipe(gulp.dest(coffeeConfig.arrUtilDest));
 });
 
-gulp.task('coffee_rootPathFile_make',function(){
+gulp.task('coffee_rootPathFile_make', function () {
 	gulp
 		.src(coffeeConfig.rootPathFileSrc)
 		.pipe(coffee())
 		.pipe(gulp.dest(coffeeConfig.rootPathFileDest));
 });
 
-gulp.task('default',['coffee_make','coffee_arrUtil_make','coffee_rootPathFile_make']);
+gulp.task('default', ['coffee_make', 'coffee_arrUtil_make', 'coffee_rootPathFile_make']);
